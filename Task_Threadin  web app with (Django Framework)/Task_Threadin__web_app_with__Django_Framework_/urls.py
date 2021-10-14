8377b31911095e85a7934e0e8ad7a6dd670a6496
Olwethu Theo Nyondo <32870672+Owzaa@users.noreply.github.com>
2021-10-14 13:35:41 -0700

    Add project files.

--- Task_Threadin  web app with (Django Framework)/Task_Threadin__web_app_with__Django_Framework_/urls.py
+++ Task_Threadin  web app with (Django Framework)/Task_Threadin__web_app_with__Django_Framework_/urls.py
@@ -8,24 +8,24 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 
-
 urlpatterns = [
-    path('',views.homeView, name='home'),
+    path('', views.home, name='home'),
+    path('submit',views.submit, name='submit'),
+    path('registerForm/', views.registerForm,name='registerForm'),
     path('currentTask/', views.currentTask, name='currentTask'),
+    path('allTask/', views.allTask, name='allTask'),
     path('createTask/', views.createTask, name='createTask'),
     path('userEditDetails/', views.userEditDetails, name='userEditDetails'),
     path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
+    path('login/',LoginView.as_view
          (
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
              extra_context=
              {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
+                 'title': 'Login',
              }
-         ),
-         name='login'),
+         ),   name='login'),
     path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('admin/', admin.site.urls),]
+    path('admin/', admin.site.urls),
+]   
